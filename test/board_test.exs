e15af5942ad5159971228aa2b2790a0c6493f410
ediw8311xht <maxballard333@gmail.com>
2024-06-21 14:35:43 -0400

    FIX

--- test/board_test.exs
+++ test/board_test.exs
@@ -11,17 +11,15 @@ defmodule BoardTest do
   test "String.Chars.to_string(%Board{})" do
     a = Board.new()
     string = to_string(a)
+    IO.puts("")
+    IO.puts(a)
     assert string
   end
 
-  test "Board.validate_move" do
-    #a = Board.new()
-    #b = Board.validate_move(a, {1, 1}, {2, 2})
-    #IO.inspect(b)
-    #assert Board.get_moves(a, {1, 1}) == []
-    #Board.get_moves(a, {3, 3})
-    #|> IO.inspect()
-    assert true
+  test "Board.all_capture/1" do
+    a = Board.new()
+    Board.all_capture(a)
+    |> IO.inspect()
   end
 
 
