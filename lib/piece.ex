22368b354e669fd09ce528eb3b60ab04813d68ae
ediw8311xht <maxballard333@gmail.com>
2024-06-26 20:21:04 -0400

    macro

--- lib/piece.ex
+++ lib/piece.ex
@@ -3,6 +3,16 @@ defmodule Piece do
   defstruct color: :empty, type: :empty, pos: :nil
   alias Generator, as: G
 
+  defmacro p({color, type}),      do: quote do: %Piece{color: unquote(color), type: unquote(type)}
+  defmacro p({color, type, pos}), do: quote do: %Piece{color: unquote(color), type: unquote(type), pos: unquote(pos)}
+
+  defmacro opposite(color) do
+    case color do
+      :black -> :red
+      :red   -> :black
+      _      -> :empty
+    end
+  end
 
   @points for y <- 1..8, x <- 1..8, into: [], do: {x, y}
   @table [
@@ -10,7 +20,6 @@ defmodule Piece do
     { {:red,    :normal } , "x" , [{1,-1},{-1,-1}]              ,  [{2,-2},{-2,-2}]              },
     { {:black,  :king   } , "O" , [{1,1},{-1,1},{1,-1},{-1,-1}] ,  [{2,2},{-2,2},{2,-2},{-2,-2}] },
     { {:red,    :king   } , "X" , [{1,1},{-1,1},{1,-1},{-1,-1}] ,  [{2,2},{-2,2},{2,-2},{-2,-2}] },
-    { {:empty,  :empty  } , "-" , []                            ,  []                            },
   ]
 
   def king_me(piece = %Piece{color: :black, type: :normal, pos: {_x, 8}}), do: {true, %{piece | type: :king}}
@@ -20,19 +29,20 @@ defmodule Piece do
   def new(color: color, type: type, pos: pos), do: %Piece{color: color, type: type, pos: pos}
 
   for {{color, type}, string, moves, captures} <- @table do
-    if color != :empty do
-      for point <- @points do
-        non_points = G.from_direction_multi(point, moves)
-        cap_points = G.from_direction_multi(point, captures)
-        def valid_moves(%Piece{color: unquote(color), type: unquote(type), pos: unquote(point)}),
-          do: unquote(non_points)
-        def valid_captures(%Piece{color: unquote(color), type: unquote(type), pos: unquote(point)}),
-          do: unquote(cap_points)
-      end
+    for point <- @points do
+      non_points = G.from_direction_multi(point, moves)
+      cap_points = G.from_direction_multi(point, captures)
+      #for [head | [middle | end] <- cap_points do
+      #  def valid_move(%Piece{color: unquote(color), type: unquote(type), pos: unquote(point),
+      #                 %Piece{color: unquote(opposite(color)), type: _, pos: unquote(head)),
+      #end
+      def valid_moves(    p(unquote({color, type, point})) ), do: unquote(non_points)
+      def valid_captures( p(unquote({color, type, point})) ), do: unquote(cap_points)
     end
-    def string(%Piece{color: unquote(color), type: unquote(type)}), do: unquote(string)
+    def string(p(unquote({color, type}))), do: unquote(string)
   end
 
+  def string(%Piece{}), do: "-"
   def valid_move(_, _), do: false
   def valid_move(_, _, capture: _), do: false
 end
